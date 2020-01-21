f8980cb77159bad58f9b303a6a77f7dbabe13baa
Andre e Mirian <mirian.iosimura@gmail.com>
2020-01-20 22:20:07 -0300

    Alteracao teste de categoria

--- spec/features/admin/admin_register_category_spec.rb
+++ spec/features/admin/admin_register_category_spec.rb
@@ -9,15 +9,15 @@ feature 'Admin register category' do
     click_on 'Categorias'
     click_on 'Criar Nova Categoria'
     fill_in 'Nome', with: 'A'
-    fill_in 'Diaria', with: 80
-    fill_in 'Seguro contra Terceiros', with: 40
-    fill_in 'Seguro do carro', with: 20
+    fill_in 'Diária padrão', with: 80
+    fill_in 'Seguro padrão contra terceiros', with: 40
+    fill_in 'Seguro padrão do carro', with: 20
     click_on 'Enviar'
 
     expect(page).to have_content('Categoria cadastrada com sucesso!')
     expect(page).to have_content('Nome: A')
-    expect(page).to have_content('Diaria: R$ 80,00')
-    expect(page).to have_content('Seguro contra Terceiros: R$ 40,00')
-    expect(page).to have_content('Seguro do carro: R$ 20,00')
+    expect(page).to have_content('Diária padrão: R$ 80,00')
+    expect(page).to have_content('Seguro padrão contra terceiros: R$ 40,00')
+    expect(page).to have_content('Seguro padrão do carro: R$ 20,00')
   end
 end
